710986010afa4e9b47c4ec288cd23fb6e5c2b9e1
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-19 21:47:03 -0400

    feat: add static files and express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import './database/connection';
+import 'express-async-errors';
 import express from 'express';
+import path from 'path';
+
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.use(routes);
 
