fe90f39fa278e4bffdae66863ccddc0c6934987f
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-19 22:13:37 -0400

    feat: add error handler and cors

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,18 @@ import './database/connection';
 import 'express-async-errors';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
+import errorHandle from './errors/handler';
 
 const app = express();
-app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
+app.use(cors());
+app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandle);
 
 app.listen(3333, () => {
   console.log('iniciando servidor');
